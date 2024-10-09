44eb996c5c40c20432f50e5a17eed22141c94fca
JOSE RUIZ LOPEZ <jose.ruiz@ieditorial.net>
2024-10-09 15:41:06 +0200

    Se ha estructurado el proyecto

---
+++ scripts/script_ejecutar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name entrenamiento_jose_ruiz                 # Nombre del proceso
+#SBATCH --partition dios   # Cola para ejecutar
+#SBATCH --gres=gpu:1                           # Numero de gpus a usar
+
+
+export PATH="/opt/anaconda/anaconda3/bin:$PATH"
+export PATH="/opt/anaconda/bin:$PATH"
+
+eval "$(conda shell.bash hook)"
+conda activate /mnt/homeGPU/joruiz/TFG/pt2.3py3.10
+
+export TFHUB_CACHE_DIR=.
+
+python dataset_loader.py > salida.txt
+
+mail -s "Proceso finalizado" ruizlopezjose@correo.ugr.es <<< "El proceso ha finalizado"
