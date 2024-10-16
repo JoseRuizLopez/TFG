d43dc10b0dc8bdba9043789fbe45f0ca7deec313
Jose Ruiz Lopez ruizlopezjose@correo.ugr.es - Resp. Daniel Molina Cabrera <joruiz@ngpu>
2024-10-16 08:22:00 +0200

    Se ha usado el generador con Mobilenet

--- src/generator.py
+++ src/generator.py
@@ -10,7 +10,7 @@ from utils.utils import plot_multiple_fitness_evolution
 if __name__ == "__main__":
     print(f"GPU: {torch.cuda.is_available()}")
     porcentajes = [10, 20, 50]
-    evaluaciones_maximas = 1
+    evaluaciones_maximas = 100
     evaluaciones_maximas_sin_mejora = 10
 
     metric: MetricList = MetricList.ACCURACY
