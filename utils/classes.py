255d84e0fbd8854a6d30e14ba9206b50c3a9e853
JOSE RUIZ LOPEZ <jose.ruiz@ieditorial.net>
2024-10-23 00:02:56 +0200

    Realizar prueba de algoritmos con 70%

--- utils/classes.py
+++ utils/classes.py
@@ -2,9 +2,9 @@ from enum import Enum
 
 
 class AlgorithmList(Enum):
-    # ALEATORIO = "aleatorio"
-    # BUSQUEDA_LOCAL = "busqueda local"
-    # GENETICO = "genetico"
+    ALEATORIO = "aleatorio"
+    BUSQUEDA_LOCAL = "busqueda local"
+    GENETICO = "genetico"
     # MEMETICO = "memetico"
     GENETICO2 = "genetico2"
 
